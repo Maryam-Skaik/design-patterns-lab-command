9954dfcee9e7a5fbc1a86124132aec4ca881156c
Maryam Skaik <mariamskaik2003@gmail.com>
2026-05-30 14:17:39 +0300

    Add CoffeeMachine class for coffee preparation

---
+++ solutions/smart-office-automation-system/CoffeeMachine.java
@@ -0,0 +1,23 @@
+package commandpattern;
+
+/*
+ * Receiver Class
+ * --------------
+ * Represents the office coffee machine.
+ */
+public class CoffeeMachine {
+
+    /*
+     * Starts coffee preparation.
+     */
+    public void start() {
+        System.out.println("Coffee Machine Started");
+    }
+
+    /*
+     * Stops coffee preparation.
+     */
+    public void stop() {
+        System.out.println("Coffee Machine Stopped");
+    }
+}
