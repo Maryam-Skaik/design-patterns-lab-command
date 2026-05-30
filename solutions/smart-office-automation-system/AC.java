83b177771c5538348f4c44e32f4dd3dc78ab1c58
Maryam Skaik <mariamskaik2003@gmail.com>
2026-05-30 14:17:17 +0300

    Add AC class for office air conditioner control

---
+++ solutions/smart-office-automation-system/AC.java
@@ -0,0 +1,23 @@
+package commandpattern;
+
+/*
+ * Receiver Class
+ * --------------
+ * Represents the office air conditioner.
+ */
+public class AC {
+
+    /*
+     * Turns the AC ON.
+     */
+    public void start() {
+        System.out.println("AC Started");
+    }
+
+    /*
+     * Turns the AC OFF.
+     */
+    public void stop() {
+        System.out.println("AC Stopped");
+    }
+}
