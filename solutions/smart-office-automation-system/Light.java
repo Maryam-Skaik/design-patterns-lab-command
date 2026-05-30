21a806a148d0c7209707a21635c6b8183ddbc4a7
Maryam Skaik <mariamskaik2003@gmail.com>
2026-05-30 14:16:27 +0300

    Create Light.java

---
+++ solutions/smart-office-automation-system/Light.java
@@ -0,0 +1,23 @@
+package commandpattern;
+
+/*
+ * Receiver Class
+ * --------------
+ * Represents the office lighting system.
+ */
+public class Light {
+
+    /*
+     * Turns the light ON.
+     */
+    public void turnOn() {
+        System.out.println("Office Light is ON");
+    }
+
+    /*
+     * Turns the light OFF.
+     */
+    public void turnOff() {
+        System.out.println("Office Light is OFF");
+    }
+}
